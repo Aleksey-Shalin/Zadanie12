06d650e2d94667c215fc9bb27874b2b7407b8402
Aleksey <aleksey_ktotamych@mail.ru>
2025-10-27 11:47:00 +0300

    Add files via upload

---
+++ Task12-1_File-output(Исправленное).cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<fstream>
+#include<string>
+#include<windows.h>
+int main()
+{
+	SetConsoleOutputCP(CP_UTF8);
+	std::string words;
+	std::ifstream file ("in.txt");
+	if (file.is_open())
+	{
+		while (file >> words)
+		{
+			std::cout << words << std::endl;
+		}
+		file.close();
+	}
+	else
+	{
+		std::cout << "Не получилось открыть файл!" << std::endl;
+	}
+	return 0;
+}
---
+++ Task12-3_User-array-to-file(Исправленное).cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<fstream>
+//#include<windows.h>
+#include<limits>
+#include<cctype>
+#include<string>
+
+int main()
+{
+	// SetConsoleOutputCP(CP_UTF8);
+	setlocale(LC_ALL, "Russian");
+	std::ofstream file("out.txt");
+	int array_size = 0;
+	while (array_size <= 0 || (std::cin.fail()))
+	{
+		std::cout << "Введите размер массива: ";
+		std::cin >> array_size;
+		if (array_size <= 0 || (std::cin.fail()))
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "Нужно ввести целое положительное число для размерности массива" << std::endl;
+			std::cout << std::endl;
+		}
+	}
+
+	int* array = new int[array_size];      // создание динамического массива для передачи элементов массива из программы в файл
+	for (int i = 0; i < array_size; i++)
+	{
+		std::cout << "arr[ " << i << " ]: ";
+		std::cin >> array[i];     // запись элементов массива для передачи в файл
+	}
+	if (file.is_open())
+	{
+		file << array_size << std::endl;    // передача длины массива в качестве первого значения в файле
+		for (int i = array_size - 1; i >= 0; i--)
+		{
+			file << array[i] << " ";       // передача элементов массива из программы в файл в обратном порядке
+		}
+	}
+	else
+	{
+		std::cout << "Не удалось записать данные в файл" << std::endl;
+	}
+	delete[] array;
+	file.close();
+	return 0;
+}
